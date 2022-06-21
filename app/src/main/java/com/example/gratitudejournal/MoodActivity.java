bb7fa688e67f0fa77b9bc81c855e7902902cfd07
Anika Patel <anikapatel@fb.com>
2022-06-21 11:03:06 -0700

    - Created mood selection activity layout - Added swipe animation from mood selection activity to compose activity (and the reverse when the back button is pressed)

--- app/src/main/java/com/example/gratitudejournal/MoodActivity.java
+++ app/src/main/java/com/example/gratitudejournal/MoodActivity.java
@@ -2,7 +2,10 @@ package com.example.gratitudejournal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 
 public class MoodActivity extends AppCompatActivity {
 
@@ -13,10 +16,34 @@ public class MoodActivity extends AppCompatActivity {
     // (which will be a button at the top of the compose activity),
     // set the mood to null before firing the intent
 
+    float x1, x2, y1, y2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood);
+    }
+
+    public boolean onTouchEvent(MotionEvent touchEvent){
+        switch(touchEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchEvent.getX();
+                y1 = touchEvent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchEvent.getX();
+                y2 = touchEvent.getY();
+
+                if(x1 > x2) {
+                Intent i = new Intent(MoodActivity.this, ComposeActivity.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                Log.i("swiped left", "it worked");
+
+                }
+            break;
+        }
+        return false;
     }
 
 }
