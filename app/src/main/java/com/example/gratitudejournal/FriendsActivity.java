ea47351be24dc400aea14b8dd898239d42c28951
Anika Patel <anikapatel@fb.com>
2022-07-08 13:22:56 -0700

    - Added expandable cardviews to the friends layout file. These cardviews will expand to show friends / close friends

--- app/src/main/java/com/example/gratitudejournal/FriendsActivity.java
+++ app/src/main/java/com/example/gratitudejournal/FriendsActivity.java
@@ -2,13 +2,113 @@ package com.example.gratitudejournal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.LayoutTransition;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import com.parse.ParseUser;
 
 public class FriendsActivity extends AppCompatActivity {
+
+    public static final String TAG = "FriendsActivity";
+
+    EditText etAddFriend;
+    EditText etAddCloseFriend;
+
+    Button btnAddFriend;
+    Button btnAddCloseFriend;
+
+    Spinner sRemoveFriend;
+    Spinner sRemoveCloseFriend;
+
+    LinearLayout llFriends;
+    TextView tvFriendsCardviewText;
+    TextView tvFriendsList;
+
+    LinearLayout llCloseFriends;
+    TextView tvCloseFriendsCardviewText;
+    TextView tvCloseFriendsList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+
+        etAddFriend = findViewById(R.id.etAddFriend);
+        etAddCloseFriend = findViewById(R.id.etAddCloseFriend);
+
+        btnAddFriend = findViewById(R.id.btnAddFriend);
+        btnAddCloseFriend = findViewById(R.id.btnAddCloseFriend);
+
+        sRemoveFriend = findViewById(R.id.sRemoveFriend);
+        sRemoveCloseFriend = findViewById(R.id.sRemoveCloseFriend);
+
+        llFriends = findViewById(R.id.llFriends);
+        tvFriendsCardviewText = findViewById(R.id.tvFriendsCardviewText);
+        tvFriendsList = findViewById(R.id.tvFriendsList);
+        llFriends.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+
+        llCloseFriends = findViewById(R.id.llCloseFriends);
+        tvCloseFriendsCardviewText = findViewById(R.id.tvCloseFriendsCardviewText);
+        tvCloseFriendsList = findViewById(R.id.tvCloseFriendsList);
+        llCloseFriends.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.new_color)));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbarmenu, menu);
+        return true;
+    }
+
+    public void onLogout(MenuItem item) {
+        ParseUser.logOut();
+        ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+        Intent intent = new Intent(FriendsActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void onHome(MenuItem item) {
+        Intent intent = new Intent(FriendsActivity.this, HomeActivity.class);
+        startActivity(intent);
+        // setVisible(false);
+        //finish();
+    }
+    public void onSettings(MenuItem item) {
+        Intent intent = new Intent(FriendsActivity.this, SettingsActivity.class);
+        startActivity(intent);
+        // setVisible(false);
+        //finish();
+    }
+
+    public void expandFriendsList(View view) {
+        int a = (tvFriendsList.getVisibility() == View.GONE)? View.VISIBLE: View.GONE;
+
+        TransitionManager.beginDelayedTransition(llFriends, new AutoTransition());
+        tvFriendsList.setVisibility(a);
+    }
+
+    public void expandCloseFriendsList(View view) {
+        int a = (tvCloseFriendsList.getVisibility() == View.GONE)? View.VISIBLE: View.GONE;
+
+        TransitionManager.beginDelayedTransition(llCloseFriends, new AutoTransition());
+        tvCloseFriendsList.setVisibility(a);
     }
 }
