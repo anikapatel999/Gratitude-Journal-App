74e1ae321a02a6911f6945474ca1ce08e707e53a
Anika Patel <anikapatel@fb.com>
2022-07-08 15:36:16 -0700

    - User can add friends

--- app/src/main/java/com/example/gratitudejournal/FriendsActivity.java
+++ app/src/main/java/com/example/gratitudejournal/FriendsActivity.java
@@ -8,6 +8,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,9 +18,22 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.example.myapplication.User;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 public class FriendsActivity extends AppCompatActivity {
 
@@ -42,6 +56,8 @@ public class FriendsActivity extends AppCompatActivity {
     TextView tvCloseFriendsCardviewText;
     TextView tvCloseFriendsList;
 
+    JSONArray friends = new JSONArray();
+    JSONArray closeFriends = new JSONArray();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +84,68 @@ public class FriendsActivity extends AppCompatActivity {
         llCloseFriends.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
 
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.new_color)));
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        User currentUser2 = (User) currentUser;
+        friends = currentUser2.getFriends();
+        closeFriends = currentUser2.getCloseFriends();
+
+        //FOR ADDING FRIENDS
+        btnAddFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etAddFriend.getText().toString();
+                ParseQuery<User> query = new ParseQuery(User.class);
+                query.whereEqualTo("username", username);
+                query.findInBackground(new FindCallback<User>() {
+                    @Override
+                    public void done(List<User> objects, ParseException e) {
+                        boolean found = false;
+                        if (objects.size() == 1){
+                            User newFriend = objects.get(0);
+                            String nfID = newFriend.getObjectId();
+                            for (int i = 0; i < friends.length(); i++) {
+                                try {
+                                    if (friends.get(i).getClass().equals(User.class)){
+                                        Log.i(TAG, "class issue");
+                                        User a = (User) friends.get(i);
+                                        String uID = a.getObjectId();
+                                        if(nfID.equals(uID)) {
+                                            found = true;
+                                            Toast.makeText(FriendsActivity.this, "This user is already in your friends list!", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                    else {
+                                        JSONObject a = (JSONObject) friends.get(i);
+                                        String id = a.getString("objectId");
+                                        Log.i(TAG, "this HeLLO " + friends);
+                                        if (nfID.equals(id)) {
+                                            Toast.makeText(FriendsActivity.this, "This user is already in your friends list!", Toast.LENGTH_SHORT).show();
+                                            found = true;
+                                            break;
+                                        }
+                                    }
+
+                                } catch (JSONException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                            if (found == false) {
+                                friends.put(newFriend);
+                                currentUser2.setFriends(friends);
+                                currentUser2.saveInBackground();
+                                Toast.makeText(FriendsActivity.this, "Friend added!", Toast.LENGTH_SHORT).show();
+                                etAddFriend.setText("");
+                            }
+                        }
+                        else {
+                            Toast.makeText(FriendsActivity.this, "User not found!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
 
     }
 
